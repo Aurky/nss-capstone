2c1d65e56ee3d7322809601a0574ef55eb73cfe2
Jonathan <daubyjs@gmail.com>
2023-12-06 16:11:42 -0600

    current work with viewing ad

--- web/src/pages/viewAd.js
+++ web/src/pages/viewAd.js
@@ -16,12 +16,21 @@ class ViewAd extends BindingClass {
     constructor() {
         super();
 
-        this.bindClassMethods(['mount'], this);
+        this.bindClassMethods(['mount', 'addAdToPage', 'clientLoaded'], this);
 
         this.dataStore = new DataStore();
         this.header = new Header(this.dataStore);
-        this.dataStore.addChangeListener(this.displaySearchResults);
+        this.dataStore.addChangeListener(this.addAdToPage);
         console.log("viewAd constructor");
+    }
+
+    async clientLoaded() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const adId = urlParams.get('ad');
+        document.getElementById('ad-name').innerText = "Loading Ad...";
+        const ad = await this.client.getAd(adId);
+        this.datastore.set('ad', ad);
+
     }
 
     mount() {
@@ -30,6 +39,17 @@ class ViewAd extends BindingClass {
 
         this.client = new BulletinBoardClient();
 
+    }
+
+    addAdToPage() {
+        const ad = this.datastore.get('ad');
+        if (ad == null) {
+            return;
+        }
+
+        document.getElementById('ad-name').innerText = ad.name;
+        document.getElementById('ad-owner').innerText = ad.userId;
+        document.getElementById('location').innerText = ad.location;
     }
 }
 
